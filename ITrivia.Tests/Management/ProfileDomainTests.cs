052e9d57db417987e6634742438d05b7b4eca57b
Felipe Ortiz Correa <ortiz.jfelipe@gmail.com>
2022-10-26 09:36:09 -0300

    Se agregan los archivos de tests para ProfileDomain, ImageDomain, LabelDomain y MessageDomain

---
+++ ITrivia.Tests/Management/ProfileDomainTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITrivia.Tests.Management
+{
+    internal class ProfileDomainTests
+    {
+    }
+}
---
+++ ITrivia.Tests/Parameter/ImageDomainTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITrivia.Tests.Parameter
+{
+    internal class ImageDomainTests
+    {
+    }
+}
